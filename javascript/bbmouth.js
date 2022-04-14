eeb431430548eed04d0eca3a3156430f2edca731
AMANJMOEZI <79466023+amanjmoezi@users.noreply.github.com>
2022-04-14 22:08:10 +0430

    add move mouth with bbmouth.js

---
+++ javascript/bbmouth.js
@@ -0,0 +1,114 @@
+
+function bBOTmouth(inp) {
+    mouthBBot.removeAttribute("style")
+    switch (inp) {
+        case 0:
+            mouthBBot.style.width="60px"
+            mouthBBot.style.height="50px"
+            break;
+        case 1:
+            mouthBBot.style.width="130px"
+            mouthBBot.style.height="40px"
+            mouthBBot.style.borderRadius = "100px"
+            break;
+        case 2:
+            mouthBBot.style.width="140px"
+            mouthBBot.style.height="40px"
+            break;
+        case 2:
+            mouthBBot.style.width="40px"
+            mouthBBot.style.height="40px"
+            break;
+        case 3:
+            mouthBBot.style.width="70px"
+            mouthBBot.style.height="40px"
+            break;
+        case 4:
+            mouthBBot.style.width="40px"
+            mouthBBot.style.height="40px"
+            mouthBBot.style.borderRadius = "100px"
+            break;
+        case 5:
+            mouthBBot.style.width="80px"
+            mouthBBot.style.height="80px"
+            mouthBBot.style.borderRadius = "100px"
+            break;
+        case 6:
+            mouthBBot.style.width="100px"
+            mouthBBot.style.height="20px"
+            mouthBBot.style.borderRadius = "0 0 10px 10px"
+            break;
+        case 7:
+            mouthBBot.style.width="100px"
+            mouthBBot.style.height="30px"
+            mouthBBot.style.borderRadius = "10px 10px 30px 30px"
+            break;
+        case 8:
+            mouthBBot.style.width="50px"
+            mouthBBot.style.height="50px"
+            break;
+        case 9:
+            mouthBBot.style.width="100px"
+            mouthBBot.style.height="50px"
+            break;
+        case 10:
+            mouthBBot.style.width="140px"
+            mouthBBot.style.height="80px"
+
+            break;
+        case 11:
+            mouthBBot.style.width="80px"
+            mouthBBot.style.height="80px"
+            mouthBBot.style.borderRadius = "20px"
+
+            break;
+        case 12:
+            mouthBBot.style.width="20px"
+            mouthBBot.style.height="50px"
+            mouthBBot.style.borderRadius = "40px"
+
+            break;
+        case 13:
+            mouthBBot.style.width="100px"
+            mouthBBot.style.height="50px"
+            mouthBBot.style.borderRadius = "40px"
+
+            break;
+        case 14:
+            mouthBBot.style.width="130px"
+            mouthBBot.style.height="10px"
+            mouthBBot.style.borderRadius = "3px"
+
+            break;
+        case 15:
+            mouthBBot.style.width="100px"
+            mouthBBot.style.height="100px"
+            mouthBBot.style.borderRadius = "10px"
+
+            break;
+        case 16:
+            mouthBBot.style.width="100px"
+            mouthBBot.style.height="100px"
+            mouthBBot.style.borderRadius = "100px"
+
+            break;
+        case 17:
+            mouthBBot.style.width="50px"
+            mouthBBot.style.height="50px"
+            break;
+        // case 18:
+        //     mouthBBot.style.width="200px"
+        //     mouthBBot.style.height="80px"
+        //     mouthBBot.style.borderRadius = "100px 100px 0 0 "
+
+        //     break;
+        
+        
+        default:
+            mouthBBot.style.width="100px"
+                mouthBBot.style.height="5px"
+        break;
+    }
+
+}
+

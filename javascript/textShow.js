588fa0e7f10eec8d2d3e7adc19ca6974fe92da23
AMANJMOEZI <79466023+amanjmoezi@users.noreply.github.com>
2022-04-15 00:58:13 +0430

    change delay text with textShow.js

--- javascript/textShow.js
+++ javascript/textShow.js
@@ -1,7 +1,8 @@
 function textShow(text) {
+  document.querySelector(".texts").innerText = "";
     types =  new Typed('.texts', {
         strings: [text],
-        typeSpeed: 0,
+        typeSpeed: 50,
         loop:false,
       });
 }

a878a13996faf381f33351c6ab1ffeb26658440d
AMANJMOEZI <79466023+amanjmoezi@users.noreply.github.com>
2022-04-14 22:09:00 +0430

    select face  with tagshtml.js

---
+++ javascript/tagsHtml.js
@@ -0,0 +1,7 @@
+const textShowsTag = document.querySelector(".texts");
+const mouthBBot = document.querySelector(".mouth");
+const BBotEyesBox = document.querySelector(".eye-box");
+const BBotEyes = document.querySelectorAll(".eye");
+const rightBBotEyes = BBotEyes[0];
+const leftBBotEyes = BBotEyes[1];
+

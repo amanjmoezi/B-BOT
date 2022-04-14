0c8208b6ed8ee1d0a3bcdb16ad6a34248697018d
AMANJMOEZI <79466023+amanjmoezi@users.noreply.github.com>
2022-04-15 00:57:06 +0430

    add laugh for reactionFace bbot  with reaction.js

--- javascript/reaction.js
+++ javascript/reaction.js
@@ -3,6 +3,27 @@ function reactionFace(inp) {
         case "story":
             
             break;
+        case "laugh":
+            new Audio("sound/5.mp3").play();
+            bBOTmouth(2)
+            setTimeout(() => {
+                bBOTmouth(10)
+            }, 100);
+            setTimeout(() => {
+                bBOTmouth(2)
+            }, 200);
+            setTimeout(() => {
+                bBOTmouth(10)
+            }, 300);
+            setTimeout(() => {
+                bBOTmouth(2)
+            }, 400);
+           
+            setTimeout(() => {
+                bBOTmouth()
+            }, 500);
+           
+            break;
     
         default:
             break;
